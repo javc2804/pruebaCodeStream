486a9b4975fe2d36bc277cd43899db8e48e0ee5d
javier villarroel <javier.villarroel@proyectokamila.com>
2022-01-25 18:36:56 -0400

    prueba

--- prueba/src/App.js
+++ prueba/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App() { // i'm test that.
   return (
     <div className="App">
       <header className="App-header">
